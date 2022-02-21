45291f428b2991ee5fbdec22cdf921b80987b771
palegreramos <palegre@iespuntadelverde.es>
2022-02-21 19:06:17 +0100

    nuevos

--- scripts.js
+++ scripts.js
@@ -5,7 +5,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
     document.querySelector(".close-btn").addEventListener("click",()=>{
         document.querySelector(".modal").classList.remove("visible");
     });
-    // document.addEventListener("click",(evt)=>{
-    //     if (evt.target.classList.has("modal")) document.querySelector(".modal").classList.remove("visible");
-    // });
+    document.addEventListener("click",(evt)=>{
+        if (evt.target.classList.contains("modal")) 
+        document.querySelector(".modal").classList.remove("visible");
+    });
 });
